78be855dfec03a22def7079cbb57e0db02142d51
gbeto19 <gbeto19@gmail.com>
2020-10-19 14:36:02 -0300

    first commit

---
+++ calculando.js
@@ -0,0 +1,24 @@
+
+function tabuada() {
+
+  let num = document.getElementById('txtn')
+  let tab = document.getElementById('seltab')
+
+  if (num.Value.length == 0) {
+    window.alert('por favor, digite um numero')
+  } else {
+    let n = number(num.value)
+    let c = 1
+    tab.innerHTML = ''
+    while (c <= 10) {
+      let item = document.createElement('option')
+      item.text = `${n} x ${c} = ${n * c}`
+      item.value = `tab${c}`
+      tab.appendChild(item)
+      c++
+    }
+  }
+
+
+}
+
